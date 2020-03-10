01405932d84956822b39706d55de927c468f3c8c
David Folk <davidwfolk@gmail.com>
2020-03-10 16:15:03 -0600

    JS & buttons work!!

--- main.js
+++ main.js
@@ -12,3 +12,169 @@ function week1() {
   document.getElementById("week-11").classList.add("hidden")
   document.getElementById("week-12").classList.add("hidden")
 }
+
+function week2() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.remove("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week3() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.remove("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week4() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.remove("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week5() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.remove("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week6() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.remove("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week7() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.remove("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week8() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.remove("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week9() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.remove("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week10() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.remove("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week11() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.remove("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
+
+function week12() {
+  document.getElementById("week-1").classList.add("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.remove("hidden")
+}
+
