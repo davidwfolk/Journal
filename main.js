618cf276692a75978af742eafbc4a9ca2ae57d8d
David Folk <davidwfolk@gmail.com>
2020-03-10 15:04:50 -0600

    flex box v1

---
+++ main.js
@@ -0,0 +1,14 @@
+function week1() {
+  document.getElementById("week-1").classList.remove("hidden")
+  document.getElementById("week-2").classList.add("hidden")
+  document.getElementById("week-3").classList.add("hidden")
+  document.getElementById("week-4").classList.add("hidden")
+  document.getElementById("week-5").classList.add("hidden")
+  document.getElementById("week-6").classList.add("hidden")
+  document.getElementById("week-7").classList.add("hidden")
+  document.getElementById("week-8").classList.add("hidden")
+  document.getElementById("week-9").classList.add("hidden")
+  document.getElementById("week-10").classList.add("hidden")
+  document.getElementById("week-11").classList.add("hidden")
+  document.getElementById("week-12").classList.add("hidden")
+}
